83cdc12c51fcfaf96d2e27b29abaf00c98fe09a2
Serhii Charykov <serhii.charykov@gmail.com>
2022-12-27 10:27:17 +0200

    Update front-end

--- forum-front/src/App.js
+++ forum-front/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,8 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" height="60" width="60"/>
-        <h1>Demo!</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
